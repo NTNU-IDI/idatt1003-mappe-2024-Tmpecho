7d8889469b37a3a366f11db9af0446a6aba466c5
Tmpecho <TMPEcho@gmail.com>
2024-12-02 23:31:33 +0100

    Add functionality to display storage detail
    Signed-off-by: Tmpecho <TMPEcho@gmail.com>

--- src/main/java/edu/ntnu/idi/idatt/view/StorageView.java
+++ src/main/java/edu/ntnu/idi/idatt/view/StorageView.java
@@ -1,14 +1,26 @@
 package edu.ntnu.idi.idatt.view;
 
 import edu.ntnu.idi.idatt.model.Grocery;
+import edu.ntnu.idi.idatt.model.Storage;
 import java.util.List;
 
 /** Class representing the view for storage. */
 public class StorageView {
   /**
+   * Display storage details.
+   *
+   * @param storage The storage to display.
+   */
+  public static void displayStorageDetails(Storage storage) {
+    System.out.println("Storage details:");
+    System.out.println("Name: " + storage.getName());
+    System.out.println("Capacity: " + storage.getCurrentCapacity() + "/" + storage.getCapacity());
+  }
+
+  /**
    * Displays all groceries in the storage as a table.
    *
-   * @param groceries the groceries to display
+   * @param groceries The groceries to display
    */
   public static void displayStorage(List<Grocery> groceries) {
     if (groceries == null || groceries.isEmpty()) {
@@ -16,13 +28,11 @@ public class StorageView {
       return;
     }
 
-    // Print table header
     System.out.printf(
         "%-15s %-10s %-10s %-15s %-10s%n",
         "Name", "Amount", "Unit", "Expiration Date", "Price (NOK)");
     System.out.println("-------------------------------------------------------------");
 
-    // Print each grocery as a table row
     groceries.forEach(GroceryView::displayGroceryAsRow);
   }
 }
