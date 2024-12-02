5c71ff0a2afb2f303c08bfe0d49772572f5def54
Tmpecho <TMPEcho@gmail.com>
2024-12-02 13:59:40 +0100

    Add tests for RecipeController and fix bug where you couldn't remove a recipe
    Signed-off-by: Tmpecho <TMPEcho@gmail.com>

--- src/main/java/edu/ntnu/idi/idatt/model/Recipe.java
+++ src/main/java/edu/ntnu/idi/idatt/model/Recipe.java
@@ -2,6 +2,7 @@ package edu.ntnu.idi.idatt.model;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /** A class representing a recipe. */
 public class Recipe {
@@ -99,5 +100,32 @@ public class Recipe {
    */
   public void setIngredients(List<Map.Entry<Grocery, Double>> ingredients) {
     this.ingredients = ingredients;
+  }
+
+  /**
+   * Checks if two Recipe objects are equal based on their name, description, and ingredients.
+   *
+   * @param o the other object to compare
+   * @return true if the recipes are equal, false otherwise
+   */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Recipe recipe = (Recipe) o;
+    return Objects.equals(name, recipe.name)
+        && Objects.equals(description, recipe.description)
+        && Objects.equals(instructions, recipe.instructions)
+        && Objects.equals(ingredients, recipe.ingredients);
+  }
+
+  /**
+   * Generates a hash code for the Recipe object based on its attributes.
+   *
+   * @return the hash code
+   */
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, description, instructions, ingredients);
   }
 }
