291c619e882665ee22c623ec4263f1b453335740
Tmpecho <TMPEcho@gmail.com>
2024-12-02 22:22:08 +0100

    Display groceries as a table
    Signed-off-by: Tmpecho <TMPEcho@gmail.com>

--- src/main/java/edu/ntnu/idi/idatt/view/GroceryView.java
+++ src/main/java/edu/ntnu/idi/idatt/view/GroceryView.java
@@ -5,15 +5,22 @@ import edu.ntnu.idi.idatt.model.Grocery;
 /** Class representing the view for groceries. */
 public class GroceryView {
   /**
-   * Displays a grocery.
+   * Displays a grocery as part of a table row.
    *
    * @param grocery the grocery to display
    */
-  public static void displayGrocery(Grocery grocery) {
+  public static void displayGroceryAsRow(Grocery grocery) {
     if (grocery == null) {
       System.out.println("Grocery not found");
       return;
     }
-    System.out.println(grocery);
+
+    System.out.printf(
+        "%-15s %-10.2f %-10s %-15s %-10.2f%n",
+        grocery.getName(),
+        grocery.getAmount(),
+        grocery.getMeasurementUnit(),
+        grocery.getExpirationDate(),
+        grocery.getPrice());
   }
 }
